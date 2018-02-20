4fef2548a422336ede3aaea60a85023ad40b81e9
carlo3247 <cawinkelhake@aol.com>
2018-02-20 13:31:30 +0000

    mock data

--- analyser.py
+++ analyser.py
@@ -4,21 +4,23 @@ class Similarity_Analyser():
     def __init__(self, gloveFile):
         self.model = loadGloveModel(gloveFile)
 
-    def compare_words(word1, word2):
-        if word1 in model and word2 in model:
+    def compare_words(self, word1, word2):
+        if word1 == word2:
+            return 1
+        if word1 in self.model and word2 in self.model:
             return np.dot(self.model[word1], self.model[word2]) / (np.linalg.norm(self.model[word1]) * np.linalg.norm(self.model[word2]))
         else:
             return 0
 
-    def compare_sentences(sentence1, sentence2):
+    def compare_sentences(self, sentence1, sentence2):
         average = 0
         for word1 in sentence1.split():
             temp_max = 0
             for word2 in sentence2.split():
-                temp_max = max(temp_max, compare_words(word1, word2))
+                temp_max = max(temp_max, self.compare_words(word1, word2))
             average += temp_max
 
-        return average / len(text1.split())
+        return average / len(word1.split())
 
 
 def loadGloveModel(gloveFile):
--- app.py
+++ app.py
@@ -7,24 +7,46 @@ from analyser import Similarity_Analyser
 
 import sys
 
-
 label_model = pickle.load(open('label_model', 'rb'))
-simliarity_model = Similarity_Analyser('glove.6b.50d.txt')
+similarity_model = Similarity_Analyser('glove.6b.50d.txt')
+
+good_match = 0.6
+maybe_match = 0.4
 
 app = Flask(__name__)
 
 class document():
-    def __init__(self, reference, title, date):
+    def __init__(self, reference, title, date, questions):
         self.reference = reference
         self.title = title
         self.date = date
+        self.questions = questions
+
+    def convert_json(self):
+        converted = { "reference": self.reference, "title": self.title, "date": self.date }
+        return converted
 
 def predict_label(question):
     return label_model.predict([question])[0]
 
+mock_documents = [ document('1','test_document','some_date',['String'])]
+
 def analyse_question(question):
     # should return tuple of document lists: first one good matches, second maybe matches
-    return ([], []) 
+
+    good = []
+    maybe = []
+    for document in mock_documents:
+        for current_question in document.questions:
+            print('{}\n{}\n\n'.format(question, current_question), file=sys.stderr)
+            temp_predict = similarity_model.compare_sentences(question, current_question)
+            print('predict: {}'.format(temp_predict), file=sys.stderr)
+            if temp_predict > good_match:
+                good.append(document)
+            elif temp_predict > maybe_match:
+                maybe.append(document)
+
+    return (good, maybe) 
 
 
 @app.route('/predict', methods = ['POST'])
@@ -36,12 +58,15 @@ def apicall():
     
     question = request_json['question']
     matching_documents = analyse_question(question)
+    print(matching_documents[0])
+    good_matches = [x.convert_json() for x in matching_documents[0]]
+    maybe_matches = [x.convert_json() for x in matching_documents[1]]
     predicted_label = predict_label(question)
 
     response_data = {
       "direct_answer": "",
-      "good_match": matching_documents[0],
-      "possible_match" : matching_documents[1],
+      "good_match": good_matches,
+      "possible_match" : maybe_matches, 
       "label": predicted_label,
     }
 
