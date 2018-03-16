255d6505c22ef1d81f1aa7458dbdcd2a2638b8f5
carlo3247 <cawinkelhake@aol.com>
2018-03-16 16:47:35 +0000

    things

--- database_manager.py
+++ database_manager.py
@@ -1,4 +1,5 @@
-
+import json
+import sys
 from analyser import Similarity_Analyser
 
 good_match = 0.8
@@ -18,55 +19,64 @@ class Document():
 class Db_Manager():
 
     def __init__(self, data_path):
-        self.documents = load_json_data(data_path)
+        self.documents = self.load_json_data(data_path)
         self.analyser = Similarity_Analyser('glove.6b.50d.txt')
 
-    def load_json_data(path):
+    def load_json_data(self, path):
         data = []
         with open(path) as json_file:
            json_data = json.load(json_file) 
-           for entry in json_data['Documents']:
+           for entry in json_data['documents']:
                data.append(Document(entry['reference'], entry['title'],entry['date'],entry['questions']))
         return data
 
     def find_matching_keywords(self, question):
-        return ""
+        matching = []
+        for entry in self.documents:
+            for current_question in entry.questions:
+                matching_keywords = self.analyser.compare_keywords(question, current_question)
+                if matching_keywords > 0:
+                    if entry not in matching:
+                        matching.append(entry)
+                        break
 
-    def find_matching_similarity(self, question):
-        return ""
 
     def find_matching_similarity_only(self, question):
-        return ""
+        good = []
+        maybe = []
+        for entry in self.documents:
+            for current_question in entry.questions:
+                predict = self.analyser.compare_sentences(question, current_question)
+                if predict > good_match:
+                    if entry not in good:
+                        good.append(entry)
+                        break
+                elif predict > maybe_match:
+                    if entry not in maybe and entry not in good:
+                        maybe.append(entry)
+                        break
+
+        return (good, maybe)
+
+    def find_matching_similarity(self, question):
+        good = []
+        maybe = []
+        for entry in self.documents:
+            for current_question in entry.questions:
+                keywords = self.analyser.compare_keywords(question, current_question)
+                if keywords > 0:
+                    predict = self.analyser.compare_sentences(question, current_question)
+#                    print('question:\n {}\n score:\n {}\n'.format(current_question, predict), file=sys.stderr)
+                    if predict > good_match:
+                        if entry not in good:
+                            good.append(entry)
+                            break
+                    elif predict > maybe_match:
+                        if entry not in maybe and entry not in good:
+                            maybe.append(entry)
+                            break
+
+        return (good, maybe)
 
     def find_direct_answer(self, question):
         return None
-
-
-# in a larger database not every single Document should be queried. Filtering the list to compare by matching keywords first, will greatly improve efficiency.
-def analyse_question(question):
-    # should return tuple of Document lists: first one good matches, second maybe matches
-    question = similarity_model.transform_string(question)
-    good = []
-    maybe = []
-    matching_keyword_questions = get_matching_keywords(question)
-    for Document in mock_Documents:
-        for current_question in Document.questions:
-            temp_predict = similarity_model.compare_sentences(question, current_question)
-            if temp_predict > good_match:
-                if Document not in good:
-                    good.append(Document)
-            elif temp_predict > maybe_match:
-                if Document not in maybe and Document not in good:
-                    maybe.append(Document)
-
-    return (good, maybe)
-
-def get_matching_keywords(question):
-    matching = []
-    for Document in mock_Documents:
-        for current_question in Document.questions:
-            matching_keywords = similarity_model.compare_keywords(question, current_question)
-            if matching_keywords > 0:
-                if Document not in matching:
-                    matching.append(Document)
-   return matching 
